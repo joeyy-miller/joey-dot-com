5466221abdfac6aa7ca05ec4fe291e3575cae02c
Joey Miller <34293717+joeyy-miller@users.noreply.github.com>
2024-07-28 12:37:07 -0700

    Added more info to config.sample.js
    + Made more clear what information is needed and what is not needed to run the web server.

--- config.sample.js
+++ config.sample.js
@@ -1,13 +1,21 @@
 // config.sample.js
 
 module.exports = {
-    openWeatherMapApiKey: 'your_openweathermap_api_key_here',
-    databaseConfig: {
+    openWeatherMapApiKey: 'your_openweathermap_api_key_here', // needed for fetching weather data
+    databaseConfig: { // needed for connecting to the database
         host: 'localhost',
         user: 'your_database_user',
         password: 'your_database_password',
         database: 'your_database_name',
     },
-    jwtSecret: 'your_jwt_secret_here',
-    // Add any other configuration variables here
+    jwtSecret: 'your_jwt_secret_here', // needed for password hasing
+    emailConfig: { // not needed. not used in current iteration
+        host: 'smtp.example.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'your_email@example.com',
+          pass: 'your_email_password'
+        }
+      }
 };
